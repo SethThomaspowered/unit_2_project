324b54cf0cc5219e7a390cc8a76582ae2fc27a2e
Seth Thomas <athomas.seth@gmail.com>
2022-01-03 21:54:25 -0500

    added UserController

---
+++ src/main/java/com/example/ultimateplaylist/controller/UserController.java
@@ -0,0 +1,9 @@
+package com.example.ultimateplaylist.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth/users")
+public class UserController{
+}
