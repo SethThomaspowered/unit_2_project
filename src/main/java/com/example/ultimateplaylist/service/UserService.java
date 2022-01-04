a36a033fae082abec2efd63e6fff0a224a3423f2
jue-lan <juelan.brown@nyu.edu>
2022-01-04 17:43:33 -0500

    deleted extra LoginRequest constructor from LoginRequest

--- src/main/java/com/example/ultimateplaylist/service/UserService.java
+++ src/main/java/com/example/ultimateplaylist/service/UserService.java
@@ -2,25 +2,20 @@ package com.example.ultimateplaylist.service;
 
 
 import com.example.ultimateplaylist.exception.InformationExistsException;
-import com.example.ultimateplaylist.exception.InformationNotFoundException;
 import com.example.ultimateplaylist.model.Request.LoginRequest;
 import com.example.ultimateplaylist.model.Response.LoginResponse;
 import com.example.ultimateplaylist.model.User;
 import com.example.ultimateplaylist.repository.UserRepository;
 import com.example.ultimateplaylist.security.JWTUtils;
-import com.example.ultimateplaylist.security.MyUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
 import java.util.logging.Logger;
 
 @Service
