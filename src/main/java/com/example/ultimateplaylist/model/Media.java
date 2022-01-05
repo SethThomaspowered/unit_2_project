83b4ed4d17a98288dd76c0b9c4bfe6e48de44a2b
SethThomaspowered <athomas.seth@gmail.com>
2022-01-05 14:48:03 -0500

    building media model

---
+++ src/main/java/com/example/ultimateplaylist/model/Media.java
@@ -0,0 +1,23 @@
+package com.example.ultimateplaylist.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "media")
+public class Media<referencedColumnName> {
+    @Id
+    @Column
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String mediaType;
+
+
+    @OneToOne
+    @JoinColumn(name = "podcast_id")
+    private Podcast podcast;
+
+    public Podcast getPodcast() {
+        return podcast;
+    }
+}
