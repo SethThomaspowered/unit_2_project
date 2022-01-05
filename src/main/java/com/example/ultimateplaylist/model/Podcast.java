d7fdcab063f4e5786d995206b71b07f3fc6be239
SethThomaspowered <athomas.seth@gmail.com>
2022-01-05 15:25:31 -0500

    added getters and setters to Podcast

--- src/main/java/com/example/ultimateplaylist/model/Podcast.java
+++ src/main/java/com/example/ultimateplaylist/model/Podcast.java
@@ -29,4 +29,74 @@ public class Podcast {
     @JoinColumn(name = "podcast_id")
     private Media media;
 
+    public Podcast(Long id, String title, String length, String releaseDate, boolean isPublic, String series, Media media) {
+        this.id = id;
+        this.title = title;
+        this.length = length;
+        this.releaseDate = releaseDate;
+        this.isPublic = isPublic;
+        this.series = series;
+        this.media = media;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLength() {
+        return length;
+    }
+
+    public void setLength(String length) {
+        this.length = length;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public boolean isPublic() {
+        return isPublic;
+    }
+
+    public void setPublic(boolean aPublic) {
+        isPublic = aPublic;
+    }
+
+    public String getSeries() {
+        return series;
+    }
+
+    public void setSeries(String series) {
+        this.series = series;
+    }
+
+    public Media getMedia() {
+        return media;
+    }
+
+    public void setMedia(Media media) {
+        this.media = media;
+    }
+
+    public Podcast() {
+
+    }
+
 }
