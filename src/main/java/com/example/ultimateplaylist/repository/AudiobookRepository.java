4a84da0be48a5a20e1c35dcdf70e8a895889ef78
SethThomaspowered <athomas.seth@gmail.com>
2022-01-06 17:30:09 -0500

    updated AudiobookRepository

--- src/main/java/com/example/ultimateplaylist/repository/AudiobookRepository.java
+++ src/main/java/com/example/ultimateplaylist/repository/AudiobookRepository.java
@@ -4,4 +4,5 @@ import com.example.ultimateplaylist.model.Audiobook;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AudiobookRepository extends JpaRepository<Audiobook, Long> {
+    Audiobook findByTitle(String title);
 }
